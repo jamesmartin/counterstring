e8b7cc95ab74289156a2dd1f61d4c024b650112b
jamesmartin <jimmymartin@gmail.com>
2010-07-16 08:09:14 +1000

    Changed signature of generate to accept length param

--- lib/CounterString.rb
+++ lib/CounterString.rb
@@ -1,25 +1,24 @@
 class CounterString
-  def initialize(length=0)
-    @length = length
+  def initialize()
     @position_marker = "*"
   end
 
-  def generate
-    if @length == 0
+  def generate(length=0)
+    if length == 0
       return ""
-    elsif @length == 1
-      return "#{@length}"
+    elsif length == 1
+      return "#{length}"
     else
       output = "2" + @position_marker
       last_counter_digit = 2
-      while output.size < @length
+      while output.size < length
         # Figure out the next counter digit
         counter_digit = output.size + (last_counter_digit.to_s.size + 1)
         if counter_digit.to_s.size > last_counter_digit.to_s.size
           counter_digit += 1
         end
         token = counter_digit.to_s + @position_marker 
-        remaining_length = @length - output.size
+        remaining_length = length - output.size
         if remaining_length < token.size + output.size 
           token = token[0,remaining_length]
         end
--- spec/counter_string_spec.rb
+++ spec/counter_string_spec.rb
@@ -2,46 +2,41 @@ $: << "../lib"
 require 'CounterString'
 
 describe CounterString do
+  before(:each) do
+    @cs = CounterString.new
+  end
     it "returns blank for empty constructor" do
-      cs = CounterString.new
-      cs.generate.should == ""
+      @cs.generate.should == ""
     end
 
     it "returns 1 for length 1" do
-      cs = CounterString.new(1)
-      cs.generate.should == "1"
+      @cs.generate(1).should == "1"
     end
 
     it "returns 2* for length 2" do
-      cs = CounterString.new(2)
-      cs.generate.should == "2*"
+      @cs.generate(2).should == "2*"
     end
 
     it "returns 2*4 for length 3" do
-      cs = CounterString.new(3)
-      cs.generate.should == "2*4"
+      @cs.generate(3).should == "2*4"
     end
 
     it "returns 2*4*6*8*1 for length 9" do
-      cs = CounterString.new(9)
-      cs.generate.should == "2*4*6*8*1"
+      @cs.generate(9).should == "2*4*6*8*1"
     end
 
     it "returns 2*4*6*8*11* for length 11" do
-      cs = CounterString.new(11)
-      cs.generate.should == "2*4*6*8*11*"
+      @cs.generate(11).should == "2*4*6*8*11*"
     end
 
     it "returns 2*4..100* for length 100" do
-      cs = CounterString.new(100)
-      string = cs.generate
+      string = @cs.generate(100)
       string.size.should == 100
       string[96,99].should == "8*10"
     end
 
     it "returns 2*4...2048 for length 2047" do
-      cs = CounterString.new(2047)
-      string = cs.generate
+      string = @cs.generate(2047)
       string.size.should == 2047
       string[2039,2046].should == "043*2048"
     end
