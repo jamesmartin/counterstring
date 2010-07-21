fd4b7167a587eec35fdb09888fed399321de5444
jamesmartin <jimmymartin@gmail.com>
2010-07-22 07:59:10 +1000

    Refactored output to a field

--- lib/CounterString.rb
+++ lib/CounterString.rb
@@ -1,6 +1,7 @@
 class CounterString
   def initialize()
     @position_marker_character = "*"
+    @output = ""
   end
 
   def generate(length=0)
@@ -9,23 +10,23 @@ class CounterString
     elsif length == 1
       return "#{length}"
     else
-      output = "2" + @position_marker_character
+      @output = "2" + @position_marker_character
       last_position_number = 2
-      while output.size < length
-        position_number = output.size + (last_position_number.to_s.size + 1)
+      while @output.size < length
+        position_number = @output.size + (last_position_number.to_s.size + 1)
         if position_number.to_s.size > last_position_number.to_s.size
           position_number += 1
         end
         token = position_number.to_s + @position_marker_character 
-        remaining_length = length - output.size
-        if remaining_length < token.size + output.size 
+        remaining_length = length - @output.size
+        if remaining_length < token.size + @output.size 
           token = token[0,remaining_length]
         end
-        output += token
+        @output += token
         last_position_number = position_number 
       end
     end
-    return output
+    return @output
   end
 
 end
