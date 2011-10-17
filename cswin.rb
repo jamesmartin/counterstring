9fd6c2a6d9015c5d09b8bfc1ab9896bc1fa4569f
Trish Khoo <trishkhoo@gmail.com>
2011-10-17 11:21:20 +1100

    Added simple command line interface for Windows and Mac for copying counterstrings to clipboard.

---
+++ cswin.rb
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'lib/CounterString'
+
+cs = CounterString.new
+
+if ARGV[0] != nil
+	output = cs.generate ARGV[0]
+	`echo #{output} | clip`
+	puts output
+else
+	puts "Please specify the string length"
+end
